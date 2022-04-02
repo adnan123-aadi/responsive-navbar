227165c99900327f2a5f6a0b8b26b70e4f5229cb
Adnan Ali <68984483+adnan123-aadi@users.noreply.github.com>
2022-04-02 18:32:06 +0500

    first commit

---
+++ javascript/app.js
@@ -0,0 +1,7 @@
+const mobile_menu=document.getElementById("mobile-menu");
+const navbar_menu=document.querySelector(".navbar-menu");
+
+mobile_menu.addEventListener("click",function(){
+    mobile_menu.classList.toggle("is-active");
+    navbar_menu.classList.toggle("active");
+});
